4dee182be4deaae38af5e45d6f72c6f363c216e6
Jacob Warner <warner.jacob@gmail.com>
2018-07-31 15:44:20 +0200

    migrate to blogdown

--- clickableMA/ui.r
+++
@@ -1,8 +0,0 @@
-shinyUI(fluidPage(
-  h1("Clickable Volcano Plots!"),
-  selectInput('inputFile', label='select a file:',choice='WT.mut'),
-  plotOutput('volcanoPlot',click='plot_click'),
-  sliderInput('cpmCut', label="log(CPM) cutoff",-10,10,-10, width="200px"),
-  #here the table for the clicked points:
-  tableOutput('clickedPoints')
-))
